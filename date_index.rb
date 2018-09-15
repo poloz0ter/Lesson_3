a4aa39f9aa39c2effcc293139a838ff1c779eb4d
poloz0ter <36717350+poloz0ter@users.noreply.github.com>
2018-09-15 20:46:16 +1100

    Update date_index.rb

--- date_index.rb
+++ date_index.rb
@@ -9,5 +9,4 @@ days_in_months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days_in_months[2] = 29 if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
 
 date_index = days_in_months[0...month].sum + day
-
 puts "Date index number is #{date_index}"
